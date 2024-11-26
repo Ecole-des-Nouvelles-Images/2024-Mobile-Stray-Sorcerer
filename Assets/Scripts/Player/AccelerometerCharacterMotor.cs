19cf145d744ab7df2457d0edd7bfc168ea4ad2b2
Charlie BOYER <charlieboyer@outlook.fr>
2024-11-26 16:24:16 +0100

    Tidy-up work environment and update namespaces

--- Assets/Scripts/Player/AccelerometerCharacterMotor.cs
+++ Assets/Scripts/Player/AccelerometerCharacterMotor.cs
@@ -4,28 +4,29 @@ using Utils;
 
 namespace Player {
     public class AccelerometerCharacterMotor : MonoBehaviour {
-        [SerializeField] private SwitchControl switchControl;
-        [SerializeField] private float xPower = 5000;
-        [SerializeField] private float yPower = 5000;
+        [SerializeField] private float speed = 500;
+        [SerializeField] private Animator animatorImportedAnimation;
         
         private Rigidbody _rb;
-
+        private Animator _animatorUnity;
+        private float _currentInputYValue;
+        private float _currentInputXValue;
         private void Awake() {
             _rb = GetComponent<Rigidbody>();
+            _animatorUnity = GetComponent<Animator>();
         }
 
         public void MouvementA(InputAction.CallbackContext ctx) {
-            //Debug.Log(ctx.ReadValue<Vector3>());
-            if (switchControl.IsAccelerometer)
-            {
-                Vector3 vec = ctx.ReadValue<Vector3>();
-                if(ctx.ReadValue<Vector3>().x > 0.15 || ctx.ReadValue<Vector3>().x < -0.15)
-                    vec.x = Mathf.Clamp(vec.x * xPower,-500f,500f);
-                if(ctx.ReadValue<Vector3>().y > 0.15 || ctx.ReadValue<Vector3>().y < -0.15)
-                    vec.y = Mathf.Clamp(vec.y *yPower, -500f,500f);
-                vec.z = 0;
-                _rb.velocity = new Vector3(vec.y*-1,vec.z,vec.x) * Time.fixedDeltaTime;
-            }
+            if (ctx.ReadValue<Vector3>().x > 0.15 || ctx.ReadValue<Vector3>().x < -0.15) _currentInputXValue = ctx.ReadValue<Vector3>().x;
+            else _currentInputXValue = 0;
+            if (ctx.ReadValue<Vector3>().y > 0.15 || ctx.ReadValue<Vector3>().y < -0.15) _currentInputYValue = ctx.ReadValue<Vector3>().y;
+            else _currentInputYValue = 0;
+            _rb.velocity = new Vector3(_currentInputYValue*-1,0,_currentInputXValue).normalized 
+                           * speed * Time.fixedDeltaTime;
+            _animatorUnity.SetFloat(AnimatorParameterAccess.VelocityX,_rb.velocity.x);
+            animatorImportedAnimation.SetFloat(AnimatorParameterAccess.VelocityX,_rb.velocity.x);
+            _animatorUnity.SetFloat(AnimatorParameterAccess.VelocityY,_rb.velocity.z);
+            animatorImportedAnimation.SetFloat(AnimatorParameterAccess.VelocityY,_rb.velocity.z);
         }
     }
 }
--- Assets/Scripts/Player/JoystickCharacterMotor.cs
+++ Assets/Scripts/Player/JoystickCharacterMotor.cs
@@ -6,25 +6,31 @@ namespace Player
 {
     public class JoystickCharacterMotor : MonoBehaviour
     {
-        [SerializeField] private SwitchControl switchControl;
-        [SerializeField] private float xPower = 600;
-        [SerializeField] private float yPower = 600;
+        [SerializeField] private float speed = 500f;
+        [SerializeField] private Animator animatorImportedAnimation;
+        
         private Rigidbody _rb;
-
+        private Animator _animatorUnity;
+        private float _currentInputYValue;
+        private float _currentInputXValue;
         private void Awake() {
             _rb = GetComponent<Rigidbody>();
+            _animatorUnity = GetComponent<Animator>();
         }
 
         public void MouvementJ(InputAction.CallbackContext ctx) {
-            if (switchControl.IsAccelerometer == false)
-            {
-                Vector2 vec = ctx.ReadValue<Vector2>();
-                if(ctx.ReadValue<Vector2>().x > 0.1 || ctx.ReadValue<Vector2>().x < -0.1)
-                    vec.x = Mathf.Clamp(vec.x * xPower,-500f,500f);
-                if(ctx.ReadValue<Vector2>().y > 0.1 || ctx.ReadValue<Vector2>().y < -0.1)
-                    vec.y = Mathf.Clamp(vec.y *yPower, -500f,500f);
-                _rb.velocity = new Vector3(vec.x,0,vec.y) * Time.fixedDeltaTime;
-            }
+            if (ctx.ReadValue<Vector2>().x > 0.15 || ctx.ReadValue<Vector2>().x < -0.15) 
+                _currentInputXValue = ctx.ReadValue<Vector2>().x;
+            else _currentInputXValue = 0;
+            if (ctx.ReadValue<Vector2>().y > 0.15 || ctx.ReadValue<Vector2>().y < -0.15) 
+                _currentInputYValue = ctx.ReadValue<Vector2>().y;
+            else _currentInputYValue = 0;
+            _rb.velocity = new Vector3(ctx.ReadValue<Vector2>().x,0,ctx.ReadValue<Vector2>().y).normalized * speed 
+                * Time.fixedDeltaTime;
+            _animatorUnity.SetFloat(AnimatorParameterAccess.VelocityX,_rb.velocity.x);
+            animatorImportedAnimation.SetFloat(AnimatorParameterAccess.VelocityX,_rb.velocity.x);
+            _animatorUnity.SetFloat(AnimatorParameterAccess.VelocityY,_rb.velocity.z);
+            animatorImportedAnimation.SetFloat(AnimatorParameterAccess.VelocityY,_rb.velocity.z);
         }
     }
 }
