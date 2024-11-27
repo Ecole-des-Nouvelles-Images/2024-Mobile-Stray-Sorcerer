94fa9f162a631d98f818ff75f5d03d472b028431
SCLKriss <117286300+SCLKskellka@users.noreply.github.com>
2024-11-27 17:17:41 +0100

    retouche accelerometre

--- Assets/Scripts/Player/AccelerometerCharacterMotor.cs
+++ Assets/Scripts/Player/AccelerometerCharacterMotor.cs
@@ -5,6 +5,7 @@ using Utils;
 namespace Player {
     public class AccelerometerCharacterMotor : MonoBehaviour {
         [SerializeField] private float speed = 500;
+        [SerializeField] private float sensibility = 0.01f;
         [SerializeField] private Animator animatorImportedAnimation;
         
         private Rigidbody _rb;
@@ -18,16 +19,21 @@ namespace Player {
         }
 
         public void MouvementA(InputAction.CallbackContext ctx) {
-            _acceltrometerValue = ctx.ReadValue<Vector3>().normalized * 10;
-            if (_acceltrometerValue.x > 0.5 || _acceltrometerValue.x < -0.5) 
-                _currentInputXValue = _acceltrometerValue.x/10;
-            else 
-                _currentInputXValue = 0;
-            if (_acceltrometerValue.y > 0.5 || _acceltrometerValue.y < -0.5)
-                _currentInputYValue = _acceltrometerValue.y/10;
-            else 
-                _currentInputYValue = 0;
-            _rb.velocity = new Vector3(_currentInputYValue*-1,0,_currentInputXValue) * speed * Time.fixedDeltaTime;
+            _acceltrometerValue = ctx.ReadValue<Vector3>().normalized;
+            Debug.Log(_acceltrometerValue);
+             if (_acceltrometerValue.x > sensibility) 
+                 _currentInputXValue = _acceltrometerValue.x;
+             if (_acceltrometerValue.x < -sensibility) 
+                 _currentInputXValue = _acceltrometerValue.x;
+             if (_acceltrometerValue.y > sensibility )
+                 _currentInputYValue = _acceltrometerValue.y;
+             if ( _acceltrometerValue.y < -sensibility)
+                 _currentInputYValue = _acceltrometerValue.y;
+           /*if(_acceltrometerValue.x < sensibility || _acceltrometerValue.x > -sensibility)
+              _currentInputXValue = 0;
+            if(_acceltrometerValue.y < sensibility || _acceltrometerValue.y > -sensibility)
+                _currentInputYValue = 0;*/
+            _rb.velocity = new Vector3(_currentInputYValue*-1,0,_currentInputXValue)*speed * Time.fixedDeltaTime;
             _animatorUnity.SetFloat(AnimatorParameterAccess.VelocityX,_rb.velocity.x);
             animatorImportedAnimation.SetFloat(AnimatorParameterAccess.VelocityX,_rb.velocity.x);
             _animatorUnity.SetFloat(AnimatorParameterAccess.VelocityY,_rb.velocity.z);
